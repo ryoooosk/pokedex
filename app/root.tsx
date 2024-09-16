6e59ab0efaef0f77829328b6d62df6e7e2dad5f7
ryoooosk <pond.well.0205@gmail.com>
2024-09-17 00:08:30 +0900

    chore: schadcn/uiをinstall

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
 } from '@remix-run/react';
 import type { LinksFunction } from '@remix-run/node';
 
-import './tailwind.css';
+import styles from './tailwind.css';
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -20,6 +20,7 @@ export const links: LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  { rel: 'stylesheet', href: styles },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
