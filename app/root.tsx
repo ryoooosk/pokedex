95496b084a5d819ae61ee524dde57eb360fc76e9
ryoooosk <pond.well.0205@gmail.com>
2024-09-21 13:56:04 +0900

    chore(root.tsx): tailwindの読み込みパスを修正

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
 } from '@remix-run/react';
 import type { LinksFunction } from '@remix-run/node';
 
-import styles from './tailwind.css';
+import styles from './tailwind.css?url';
 
 export const links: LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
