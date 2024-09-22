541331df0360c64dbe9f5ab3b8392b929d23e387
ryoooosk <pond.well.0205@gmail.com>
2024-09-22 18:50:58 +0900

    feat: footerを実装

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center h-12 border-t-2 border-yellow-400">
+      <p className="text-sm">&copy; 2024 Pokedex demo</p>
+    </footer>
+  );
+}
